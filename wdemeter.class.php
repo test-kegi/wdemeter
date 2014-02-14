0f2aa632328ea74943f6d2f611ba80d537811d0d
Kevin <kevin@kegisiteweb.com>
2014-02-13 23:26:22 -0500

    wdemeter class created

---
+++ wdemeter.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: WDemeter
+ * Description: WDemeter is a mini-framework for wordpress plugins
+ * Version: 0.1.0
+ * Author: KeGi Site Web
+ * Author URI: http://www.kegisiteweb.com
+ */
+
+abstract class wdemeter{
+
+	/**
+	 * WDemeter constructor (needs to be extended from a plugin main class)
+	 */
+	function __construct(){
+	
+	}
+}
